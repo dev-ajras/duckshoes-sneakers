3399f3aea1f929bb0dde3844913e33c20fd280a5
Tobias <tobyajras@gmail.com>
2023-07-19 23:43:39 -0300

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 function Login() {
   return (
     <section>
       <h3 className="font-bold text-lg pt-2 mx-3">Login</h3>
     </section>
-  )
+  );
 }
 
-export default Login
+export default Login;
