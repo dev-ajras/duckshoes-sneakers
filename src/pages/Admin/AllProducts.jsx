da4ab7a0723401c73dae2ad191c2405ea5522e5e
dev-ajras <dev.ajras@gmail.com>
2023-08-31 16:23:35 -0300

    change to page 1

--- src/pages/Admin/AllProducts.jsx
+++ src/pages/Admin/AllProducts.jsx
@@ -1,23 +1,23 @@
-import { useContext, useEffect, useState } from 'react';
-import axios from 'axios';
-import { BiSolidEdit } from 'react-icons/bi';
-import { MdDelete } from 'react-icons/md';
-import { Link } from 'react-router-dom';
-import { AppContext } from '../../context/AppProvider';
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { BiSolidEdit } from "react-icons/bi";
+import { MdDelete } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { AppContext } from "../../context/AppProvider";
 
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function AllProducts() {
   const { user } = useContext(AppContext);
 
   const [adminProducts, setAdminProducts] = useState([]);
 
-  const baseUrl = 'https://www.api.duckshoes.com.ar/';
+  const baseUrl = "https://www.api.duckshoes.com.ar/";
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await axios.get(baseUrl + 'products?page=2');
+      const response = await axios.get(baseUrl + "products?page=1");
       console.log(response);
       setAdminProducts(response.data.products);
     };
@@ -25,7 +25,7 @@ function AllProducts() {
   }, []);
 
   const productDeleted = () =>
-    toast.success('Producto Eliminado', {
+    toast.success("Producto Eliminado", {
       autoClose: 2000,
       hideProgressBar: true,
       pauseOnFocusLoss: false,
@@ -34,7 +34,7 @@ function AllProducts() {
 
   const deleteProduct = async (productId) => {
     try {
-      const response = await axios.delete(baseUrl + 'products/' + productId, {
+      const response = await axios.delete(baseUrl + "products/" + productId, {
         headers: {
           Authorization: `Bearer ${user.token}`,
         },
@@ -52,49 +52,49 @@ function AllProducts() {
   };
 
   return (
-    <div className='shadow'>
+    <div className="shadow">
       <ToastContainer />
-      <div className='grid grid-cols-9 bg-gray-50 p-5 font-normal'>
-        <div className='text-center col-span-2'>Imágen</div>
-        <div className='text-center col-span-2'>Sku</div>
-        <div className='text-center col-span-2'>Precio</div>
-        <div className='text-center col-span-2'>Estado</div>
+      <div className="grid grid-cols-9 bg-gray-50 p-5 font-normal">
+        <div className="text-center col-span-2">Imágen</div>
+        <div className="text-center col-span-2">Sku</div>
+        <div className="text-center col-span-2">Precio</div>
+        <div className="text-center col-span-2">Estado</div>
       </div>
       {adminProducts
         ? adminProducts.map((product) => (
             <div
               key={product.id}
-              className='grid grid-cols-9 place-items-center bg-white p-2 mb-0.5'
+              className="grid grid-cols-9 place-items-center bg-white p-2 mb-0.5"
             >
-              <div className='flex justify-center items-center col-span-2'>
+              <div className="flex justify-center items-center col-span-2">
                 <img
-                  className='w-20 h-20 object-contain'
+                  className="w-20 h-20 object-contain"
                   src={product.image}
                   alt={product.sku}
                 />
               </div>
-              <div className='text-center col-span-2'>{product.sku}</div>
-              <div className='text-center col-span-2'>{product.price}</div>
-              <div className='text-center col-span-2'>
+              <div className="text-center col-span-2">{product.sku}</div>
+              <div className="text-center col-span-2">{product.price}</div>
+              <div className="text-center col-span-2">
                 {product.active && (
-                  <div className='flex justify-evenly items-center'>
-                    <span className='bg-green-100 p-3 text-green-800 rounded'>
+                  <div className="flex justify-evenly items-center">
+                    <span className="bg-green-100 p-3 text-green-800 rounded">
                       Publicado
                     </span>
                   </div>
                 )}
               </div>
-              <div className='flex gap-3'>
+              <div className="flex gap-3">
                 <Link to={`/admin/editar-producto/${product.id}`}>
-                  <BiSolidEdit className='w-7 h-7' />
+                  <BiSolidEdit className="w-7 h-7" />
                 </Link>
                 <button onClick={() => deleteProduct(product.id)}>
-                  <MdDelete className='w-7 h-7 fill-red-600' />
+                  <MdDelete className="w-7 h-7 fill-red-600" />
                 </button>
               </div>
             </div>
           ))
-        : ''}
+        : ""}
     </div>
   );
 }
