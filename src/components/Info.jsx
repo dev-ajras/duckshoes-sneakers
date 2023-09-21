485b1e365c408798bcba16b69853343b83312281
Tobias <tobyajras@gmail.com>
2023-09-20 23:58:07 -0300

    product details pre

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,50 +1,45 @@
-import creditCard from '/assets/info/duckCreditCard.svg';
-import shipping from '/assets/info/duckShipping.svg';
-import secure from '/assets/info/duckSecure.svg';
-import { FaRegHandPointer } from 'react-icons/fa';
-
 function Info() {
   return (
-    <div className='bg-white flex justify-center w-full my-3 md:my-5 md:h-96'>
-      <div className='flex flex-col my-3 sm:my-5 md:flex-row md:items-center'>
-        <div className='flex flex-col items-center p-8 sm:p-12 md:p-5 lg:max-w-6xl basis-1/3'>
+    <div className="bg-white flex justify-center w-full my-3 md:my-5 md:h-96">
+      <div className="flex flex-col my-3 sm:my-5 md:flex-row md:items-center">
+        <div className="flex flex-col items-center p-8 sm:p-12 md:p-5 lg:max-w-6xl basis-1/3">
           <img
-            className='w-16 sm:w-24'
-            src='/assets/info/duckshoes-carrito.svg'
-            alt='shipping info'
+            className="w-16 sm:w-24"
+            src="/assets/info/duckshoes-carrito.svg"
+            alt="shipping info"
           />
-          <h4 className='font-medium text-center mt-3 sm:text-2xl sm:mt-5'>
+          <h4 className="font-medium text-center mt-3 sm:text-2xl sm:mt-5">
             Añadir al carrito
           </h4>
-          <p className='text-center sm:text-xl sm:mt-2'>
+          <p className="text-center sm:text-xl sm:mt-2">
             Selecciona tus productos
           </p>
         </div>
-        <span className='flex m-auto bg-grayDuck h-[3px] w-32 sm:w-48 sm:h-1 md:w-[3px] md:h-20'></span>
-        <div className='flex flex-col items-center p-8 sm:p-12 md:p-5 basis-1/3'>
+        <span className="flex m-auto bg-grayDuck h-[3px] w-32 sm:w-48 sm:h-1 md:w-[3px] md:h-20"></span>
+        <div className="flex flex-col items-center p-8 sm:p-12 md:p-5 basis-1/3">
           <img
-            className='w-16 sm:w-24'
-            src='/assets/info/duckshoes-notificacion.svg'
-            alt='shipping info'
+            className="w-16 sm:w-24"
+            src="/assets/info/duckshoes-notificacion.svg"
+            alt="shipping info"
           />
-          <h4 className='font-medium text-center mt-3 sm:text-2xl sm:mt-5'>
+          <h4 className="font-medium text-center mt-3 sm:text-2xl sm:mt-5">
             Notificaciones
           </h4>
-          <p className='text-center sm:text-xl sm:mt-2'>
+          <p className="text-center sm:text-xl sm:mt-2">
             Seguimiento durante todo el proceso
           </p>
         </div>
-        <span className='flex m-auto bg-grayDuck h-[3px] w-32 sm:w-48 sm:h-1 md:w-[3px] md:h-20'></span>
-        <div className='flex flex-col items-center p-8 sm:p-12 md:p-5 basis-1/3'>
+        <span className="flex m-auto bg-grayDuck h-[3px] w-32 sm:w-48 sm:h-1 md:w-[3px] md:h-20"></span>
+        <div className="flex flex-col items-center p-8 sm:p-12 md:p-5 basis-1/3">
           <img
-            className='w-16 sm:w-24'
-            src='/assets/info/duckshoes-despachado.svg'
-            alt='secure info'
+            className="w-16 sm:w-24"
+            src="/assets/info/duckshoes-despachado.svg"
+            alt="secure info"
           />
-          <h4 className='font-medium text-center mt-3 sm:text-2xl sm:mt-5'>
+          <h4 className="font-medium text-center mt-3 sm:text-2xl sm:mt-5">
             Pedido realizado
           </h4>
-          <p className='text-center sm:text-xl sm:mt-2'>
+          <p className="text-center sm:text-xl sm:mt-2">
             Listo! Lo estamos preparando
           </p>
         </div>
--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,19 +1,19 @@
-import { useEffect, useState, useContext, useRef } from 'react';
-import { Link, useParams, useSearchParams } from 'react-router-dom';
-import { AppContext } from '../context/AppProvider';
-import { toast, ToastContainer } from 'react-toastify';
+import { useEffect, useState, useContext, useRef } from "react";
+import { Link, useParams, useSearchParams } from "react-router-dom";
+import { AppContext } from "../context/AppProvider";
+import { toast, ToastContainer } from "react-toastify";
 
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from "react-router-dom";
 
-import CarouselFeatured from './CarouselFeatured';
-import { BsShare } from 'react-icons/bs';
-import { MdFavoriteBorder } from 'react-icons/md';
-import { AiFillHeart } from 'react-icons/ai';
-import axios from 'axios';
-import SkeletonProductsDetails from './Skeletons/SkeletonProductsDetails';
-import { IoReturnDownBack } from 'react-icons/io5';
+import CarouselFeatured from "./CarouselFeatured";
+import { BsShare } from "react-icons/bs";
+import { MdFavoriteBorder } from "react-icons/md";
+import { AiFillHeart } from "react-icons/ai";
+import axios from "axios";
+import SkeletonProductsDetails from "./Skeletons/SkeletonProductsDetails";
+import { IoReturnDownBack } from "react-icons/io5";
 
 function ProductDetails() {
   const { favorites, cart, cartAdd, favoritesHandler } = useContext(AppContext);
@@ -21,13 +21,13 @@ function ProductDetails() {
 
   const [searchParams, setSearchParams] = useSearchParams();
   const { productId } = useParams();
-  const colorParam = searchParams.get('color');
+  const colorParam = searchParams.get("color");
   const [productFound, setProductFound] = useState({});
   const [principalImage, setPrincipalImage] = useState(0);
 
-  const baseUrl = 'https://www.api.duckshoes.com.ar/';
-  console.log('colorParam', colorParam);
-  console.log('productId', productId);
+  const baseUrl = "https://www.api.duckshoes.com.ar/";
+  console.log("colorParam", colorParam);
+  console.log("productId", productId);
 
   useEffect(() => {
     setLoading(true);
@@ -68,7 +68,7 @@ function ProductDetails() {
       )
     ) {
       if (!toast.isActive(addToCartRef.current)) {
-        addToCartRef.current = toast.info('Este producto ya esta agregado', {
+        addToCartRef.current = toast.info("Este producto ya esta agregado", {
           autoClose: 1000,
           hideProgressBar: true,
           pauseOnFocusLoss: false,
@@ -77,7 +77,7 @@ function ProductDetails() {
       }
     } else {
       if (!toast.isActive(addToCartRef.current)) {
-        addToCartRef.current = toast.success('Agregado al carrito', {
+        addToCartRef.current = toast.success("Agregado al carrito", {
           autoClose: 1000,
           hideProgressBar: true,
           pauseOnFocusLoss: false,
@@ -99,85 +99,87 @@ function ProductDetails() {
       (imageUrl, idx) => idx === principalImage
     );
 
-  console.log('productFound: ', productFound);
+  console.log("productFound: ", productFound);
 
   return (
-    <article className='flex justify-center'>
-      <ToastContainer className='mt-24' />
-      <div className='flex flex-col items-center w-full'>
-        <div className='w-full flex justify-center p-3 sm:p-5'>
-          <div className='lg:max-w-6xl w-full'>
+    <article className="flex justify-center">
+      <ToastContainer className="mt-24" />
+      <div className="flex flex-col items-center w-full">
+        <div className="w-full flex justify-center p-3 sm:p-5">
+          <div className="lg:max-w-6xl w-full">
             <button
               onClick={() => returnPrev()}
-              className='flex justify-center items-center font-medium text-lg mb-3 sm:mb-5 sm:text-2xl bg-primaryDark md:hover:bg-primaryExtraDark md:transition-colors rounded-sm p-2 w-10 h-10 '
+              className="flex justify-center items-center font-medium text-lg mb-3 sm:mb-5 sm:text-2xl bg-primaryDark md:hover:bg-primaryExtraDark md:transition-colors rounded-sm p-2 w-10 h-10 "
             >
-              <IoReturnDownBack className='stroke-white w-full h-full' />
+              <IoReturnDownBack className="stroke-white w-full h-full" />
             </button>
             {loading === true ? (
               <SkeletonProductsDetails />
             ) : productFound.id == productId ? (
-              <div className='p-3 sm:p-5 bg-white rounded-md relative'>
-                <div className='absolute top-3 left-3 sm:top-5 sm:left-5 flex flex-col gap-3 sm:gap-5 z-10'>
+              <div className="p-3 sm:p-5 bg-white rounded-md relative">
+                <div className="absolute top-3 left-3 sm:top-5 sm:left-5 flex flex-col gap-3 sm:gap-5 z-10">
                   {productFound.images &&
                     productFound.images[colorParam].map((imageUrl, idx) => (
                       <button
                         key={idx}
                         className={`h-12 sm:h-16 w-12 sm:w-16 p-2 md:p-2 rounded-sm bg-white ${
                           principalImage == idx
-                            ? 'ring-blue-500 ring-2'
-                            : 'ring-gray-500 ring-1'
+                            ? "ring-blue-500 ring-2"
+                            : "ring-gray-500 ring-1"
                         }`}
                         onMouseEnter={() => setPrincipalImage(idx)}
                         onClick={() => setPrincipalImage(idx)}
                       >
                         <img
-                          className='bg-white w-full h-full  object-contain mx-auto -z-20'
+                          className="bg-white w-full h-full  object-contain mx-auto -z-20"
                           src={imageUrl}
                           alt={idx}
                         />
                       </button>
                     ))}
                 </div>
-                <div className='flex flex-col md:flex-row md:gap-10'>
-                  <div className='relative md:basis-2/3'>
-                    <div className='flex justify-center sm:mb-3 md:mb-5 ml-16'>
+                <div className="flex flex-col md:flex-row md:gap-10">
+                  <div className="relative md:basis-2/3">
+                    <div className="flex justify-center sm:mb-3 md:mb-5 ml-16">
                       <img
-                        className='h-56 sm:h-80 object-contain px-5 py-10 sm:px-20 md:px-14 lg:px-28'
+                        className="h-56 sm:h-80 object-contain px-5 py-10 sm:px-20 md:px-14 lg:px-28"
                         src={productFoundOne}
                         alt={productFoundOne}
                       />
                     </div>
-                    <div className='hidden md:block w-20'>
-                      <h3 className='font-semibold sm:text-xl'>Descripción:</h3>
-                      <pre className='font-medium opacity-80 mr-24'>
+                    <div className="hidden md:block">
+                      <h3 className="font-semibold sm:text-xl mb-2">
+                        Descripción:
+                      </h3>
+                      <pre className="font-outfit font-normal opacity-80 mr-24 whitespace-pre-wrap">
                         {productFound.description}
                       </pre>
                     </div>
                   </div>
-                  <div className='md:basis-1/3  md:w-full'>
-                    <div className='md:sticky md:top-28 my-3 md:mb-0'>
-                      <h2 className='font-semibold sm:text-lg line-clamp-2 '>
+                  <div className="md:basis-1/3  md:w-full">
+                    <div className="md:sticky md:top-28 my-3 md:mb-0">
+                      <h2 className="font-semibold sm:text-lg line-clamp-2 ">
                         <span>SKU: </span>
-                        <span className='opacity-80'>
+                        <span className="opacity-80">
                           {productFound.sku && productFound.sku.toUpperCase()}
                         </span>
                       </h2>
-                      <span className='block my-3 font-semibold text-3xl sm:text-4xl'>
-                        ${parseInt(productFound.price).toLocaleString('es-ES')}
+                      <span className="block my-3 font-semibold text-3xl sm:text-4xl">
+                        ${parseInt(productFound.price).toLocaleString("es-ES")}
                       </span>
-                      <div className='my-3 font-semibold sm:text-lg'>
+                      <div className="my-3 font-semibold sm:text-lg">
                         <span>Temporada: </span>
-                        <span className='opacity-80 '>
+                        <span className="opacity-80 ">
                           {productFound.temporada && productFound.temporada}
                         </span>
                       </div>
                       {productFound.color && (
                         <div>
-                          <h3 className='font-semibold mt-2 sm:text-lg'>
+                          <h3 className="font-semibold mt-2 sm:text-lg">
                             <span>Color: </span>
-                            <span className='opacity-80'>{colorParam}</span>
+                            <span className="opacity-80">{colorParam}</span>
                           </h3>
-                          <div className='flex gap-5'>
+                          <div className="flex gap-5">
                             {productFound.images &&
                               Object.keys(productFound.images).map(
                                 (color, idx) => (
@@ -188,12 +190,12 @@ function ProductDetails() {
                                     to={`/products/${productFound.sku}/${productFound.id}?color=${color}`}
                                     className={`${
                                       color == colorParam
-                                        ? 'ring ring-blue-500 ring-offset-2 w-10 rounded-full  my-3 sm:ring-offset-4'
-                                        : 'ring-gray-500 ring-1 w-10 rounded-full  my-3 sm:ring-offset-4'
+                                        ? "ring ring-blue-500 ring-offset-2 w-10 rounded-full  my-3 sm:ring-offset-4"
+                                        : "ring-gray-500 ring-1 w-10 rounded-full  my-3 sm:ring-offset-4"
                                     }`}
                                   >
                                     <img
-                                      className='drop-shadow-md rounded-full '
+                                      className="drop-shadow-md rounded-full "
                                       src={`/assets/colors/${color.toLowerCase()}Color.svg`}
                                       alt={color}
                                     />
@@ -203,9 +205,9 @@ function ProductDetails() {
                           </div>
                         </div>
                       )}
-                      <div className='flex'>
+                      <div className="flex">
                         <button
-                          className='box-border w-full bg-primaryDark md:hover:bg-primaryExtraDark md:transition-colors text-white text-lg font-semibold my-3 p-2 sm:my-5 sm:p-3 rounded-sm'
+                          className="box-border w-full bg-primaryDark md:hover:bg-primaryExtraDark md:transition-colors text-white text-lg font-semibold my-3 p-2 sm:my-5 sm:p-3 rounded-sm"
                           onClick={() => {
                             handleButton(productFound);
                           }}
@@ -213,44 +215,44 @@ function ProductDetails() {
                           Agregar al carrito
                         </button>
                       </div>
-                      <div className='flex mb-3 gap-3 sm:gap-5'>
+                      <div className="flex mb-3 gap-3 sm:gap-5">
                         <button
                           onClick={() => favoritesHandler(productFound.id)}
-                          className='w-full bg-primaryLight md:hover:bg-primary md:transition-colors p-1 flex justify-center items-center gap-2 sm:p-2 sm:gap-3 font-bold rounded-sm'
+                          className="w-full bg-primaryLight md:hover:bg-primary md:transition-colors p-1 flex justify-center items-center gap-2 sm:p-2 sm:gap-3 font-bold rounded-sm"
                         >
                           {favorites.includes(productFound.id) ? (
-                            <AiFillHeart className='fill-red-600 sm:w-5 sm:h-5' />
+                            <AiFillHeart className="fill-red-600 sm:w-5 sm:h-5" />
                           ) : (
-                            <MdFavoriteBorder className='sm:w-5 sm:h-5 ' />
+                            <MdFavoriteBorder className="sm:w-5 sm:h-5 " />
                           )}
-                          <span className='opacity-80'>Favoritos</span>
+                          <span className="opacity-80">Favoritos</span>
                         </button>
                         <a
                           href={`https://api.whatsapp.com/send?phone=541138596093&text=Hola! Quería consulta por las zapatillas ${productFound.sku} | Color: ${colorParam}`}
-                          className='w-full bg-primaryLight md:hover:bg-primary md:transition-colors p-1 flex justify-center items-center gap-2 sm:p-2 sm:gap-3 font-bold rounded-sm'
+                          className="w-full bg-primaryLight md:hover:bg-primary md:transition-colors p-1 flex justify-center items-center gap-2 sm:p-2 sm:gap-3 font-bold rounded-sm"
                         >
-                          <BsShare className='sm:w-5 sm:h-5' />
-                          <span className='opacity-80'>Compartir</span>
+                          <BsShare className="sm:w-5 sm:h-5" />
+                          <span className="opacity-80">Compartir</span>
                         </a>
                       </div>
-                      <div className='md:hidden'>
-                        <h3 className='font-semibold sm:text-xl'>
+                      <div className="md:hidden">
+                        <h3 className="font-semibold sm:text-xl mb-1 md:mb-2 mt-5">
                           Descripción:
                         </h3>
-                        <p className='font-medium opacity-80 mr-24'>
+                        <pre className="font-outfit font-normal opacity-80 whitespace-pre-wrap">
                           {productFound.description}
-                        </p>
+                        </pre>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             ) : (
-              ''
+              ""
             )}
           </div>
         </div>
-        <CarouselFeatured title={'Productos Relacionados'} />
+        <CarouselFeatured title={"Productos Relacionados"} />
       </div>
     </article>
   );
