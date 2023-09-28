3ec28f83e5aad79e20287ec8bfecad18242f1daf
Tobias <tobyajras@gmail.com>
2023-09-28 00:55:01 -0300

    restablecer contrasena form added

--- src/pages/RestablecerContraseña.jsx
+++ src/pages/RestablecerContraseña.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
-import { MdMail } from 'react-icons/md';
+import { MdMail } from "react-icons/md";
+import axios from "axios";
+import { ImSpinner8 } from "react-icons/im";
 
 function RestablecerContraseña() {
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
   const [emailOk, setEmailOk] = useState(false);
-
-  const emailSent = () =>
-    toast.success('Correo de recuperación enviado', {
-      autoClose: 2000,
-      hideProgressBar: true,
-      pauseOnFocusLoss: false,
-      pauseOnHover: false,
-    });
+  const [loading, setLoading] = useState(false);
 
   const emailUserError = () =>
-    toast.error('Correo electrónico incorrecto', {
-      autoClose: 2000,
+    toast.error("Correo electrónico incorrecto", {
+      autoClose: 3000,
       hideProgressBar: true,
       pauseOnFocusLoss: false,
       pauseOnHover: false,
     });
 
   const emailServerError = () =>
-    toast.error('Error al intentar recuperar la cuenta', {
-      autoClose: 2000,
+    toast.error("Error al intentar recuperar la cuenta", {
+      autoClose: 3000,
       hideProgressBar: true,
       pauseOnFocusLoss: false,
       pauseOnHover: false,
@@ -39,59 +34,82 @@ function RestablecerContraseña() {
     postRecover();
   };
 
-  const postRecover = () => {
-    console.log('postRecover');
+  const baseUrl = "https://www.api.duckshoes.com.ar/";
+
+  const postRecover = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.post(`${baseUrl}users/recover-account`, {
+        email,
+      });
+      console.log(response);
+      if (response.status === 200) {
+        setEmailOk(true);
+      }
+    } catch (error) {
+      if (error.response.status === 400) {
+        emailUserError();
+      } else {
+        emailServerError();
+      }
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <section className='flex justify-center m-7 sm:m-14'>
+    <section className="flex justify-center m-7 sm:m-14">
       {!emailOk ? (
-        <article className=' bg-white rounded-md shadow p-10 w-96'>
+        <article className=" bg-white rounded-md shadow p-10 w-96">
           <ToastContainer />
-          <div className='mb-5 border-b-2 pb-2 border-primaryDark'>
-            <h3 className='font-medium text-2xl text-primaryDark'>
+          <div className="mb-5 border-b-2 pb-2 border-primaryDark">
+            <h3 className="font-medium text-2xl text-primaryDark">
               Restablecer contraseña
             </h3>
-            <h4 className='font text-lg'>Introducir correo electrónico</h4>
+            <h4 className="font text-lg">Introducir correo electrónico</h4>
           </div>
-          <form onSubmit={handleForm} className='flex flex-col'>
-            <label className='text-sm' htmlFor='email'>
+          <form onSubmit={handleForm} className="flex flex-col">
+            <label className="text-sm" htmlFor="email">
               email
             </label>
             <input
               value={email}
               onChange={(e) => {
-                setFormData(setEmail(e.target.value));
+                setEmail(e.target.value);
               }}
               required
-              id='email'
-              type='email'
-              placeholder='Correo electrónico'
-              className='mb-5 border-b outline-none py-1'
+              id="email"
+              type="email"
+              placeholder="Correo electrónico"
+              className="mb-5 border-b outline-none py-1"
             />
 
-            <button className='md:hover:bg-primaryExtraDark md:transition-colors bg-primaryDark p-2 text-white rounded font-normal'>
-              Restablecer
+            <button className="flex justify-center items-center md:hover:bg-primaryExtraDark md:transition-colors bg-primaryDark p-2 text-white rounded font-normal">
+              {loading ? (
+                <ImSpinner8 className="animate-spin w-7 h-7" />
+              ) : (
+                "Restablecer"
+              )}
             </button>
           </form>
         </article>
       ) : (
-        <article className='flex justify-center'>
-          <div className='m-3 sm:m-5 max-w-6xl w-full'>
-            <h3 className='text-center font-bold text-lg mb-3 sm:mb-5 sm:text-2xl'>
-              Correo de recuperación enviado!
+        <article className="flex justify-center">
+          <div className="m-3 sm:m-5 max-w-6xl w-full">
+            <h3 className="text-center font-semibold text-lg mb-3 sm:mb-5 sm:text-2xl">
+              Link de recuperación enviado!
             </h3>
-            <div className='flex flex-col items-center'>
+            <div className="flex flex-col items-center">
               <motion.div
                 initial={{ scale: 0 }}
                 animate={{ scale: 1 }}
-                transition={{ type: 'spring', stiffness: 100 }}
-                className='w-28 h-28 md:w-36 md:h-36 bg-sky-700 p-5 mt-5 mb-3 rounded-full'
+                transition={{ type: "spring", stiffness: 100 }}
+                className="w-28 h-28 md:w-36 md:h-36 bg-sky-700 p-5 mt-5 mb-3 rounded-full"
               >
-                <MdMail className='fill-white w-full h-full' />
+                <MdMail className="fill-white w-full h-full" />
               </motion.div>
-              <h4 className='sm:text-xl font-semibold mt-2'>
-                Verifica tu bandeja de entrada
+              <h4 className="sm:text-xl font-semibold mt-2">
+                Verifica tu bandeja de correo
               </h4>
             </div>
           </div>
--- src/pages/RestablecerContraseñaForm.jsx
+++ src/pages/RestablecerContraseñaForm.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useContext, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { AppContext } from "../context/AppProvider";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
-import { AiFillCheckCircle } from 'react-icons/ai';
+import { AiFillCheckCircle } from "react-icons/ai";
+import axios from "axios";
 
 function RestablecerContraseñaForm() {
-  const { token, userId } = useParams();
+  const { user } = useContext(AppContext);
 
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
   const [isRepeatPasswordValid, setIsRepeatPasswordValid] = useState(false);
   const [onBlurRepeatPassword, setOnBlurRepeatPassword] = useState(false);
-  const [emailOk, setEmailOk] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const validateRepeatPassword = (input) => {
     return password === input;
@@ -26,25 +28,8 @@ function RestablecerContraseñaForm() {
     setIsRepeatPasswordValid(validateRepeatPassword(currentRepeatPassword));
   };
 
-  const handleForm = (e) => {
-    e.preventDefault();
-    postChange();
-  };
-
-  const postChange = () => {
-    console.log('post');
-  };
-
-  const emailSent = () =>
-    toast.success('Contraseña restaurada con éxito', {
-      autoClose: 2000,
-      hideProgressBar: true,
-      pauseOnFocusLoss: false,
-      pauseOnHover: false,
-    });
-
-  const emailUserError = () =>
-    toast.error('Correo electrónico inválido', {
+  const emailSuccessAlert = () =>
+    toast.success("Contraseña restaurada con éxito", {
       autoClose: 2000,
       hideProgressBar: true,
       pauseOnFocusLoss: false,
@@ -52,90 +37,107 @@ function RestablecerContraseñaForm() {
     });
 
   const emailServerError = () =>
-    toast.error('Error al intentar recuperar la contraseña', {
+    toast.error("Error al intentar restaurar la cuenta", {
       autoClose: 2000,
       hideProgressBar: true,
       pauseOnFocusLoss: false,
       pauseOnHover: false,
     });
 
-  console.log('toke :', token);
-  console.log('userId :', userId);
+  const handleForm = (e) => {
+    e.preventDefault();
+    postChange();
+  };
+
+  const baseUrl = "https://www.api.duckshoes.com.ar/";
+
+  const navigate = useNavigate();
+
+  const postChange = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.post(
+        `${baseUrl}users/change-password`,
+        {
+          password: password,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+      if (response.status === 200) {
+        emailSuccessAlert();
+        setTimeout(() => {
+          navigate("/login", { replace });
+        }, 4000);
+      }
+    } catch (error) {
+      if (error.response) {
+        emailServerError();
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <section className='flex justify-center m-7 sm:m-14'>
-      {!emailOk ? (
-        <article className=' bg-white rounded-md shadow p-10 w-96'>
-          <ToastContainer />
-          <div className='mb-5 border-b-2 pb-2 border-primaryDark'>
-            <h3 className='font-medium text-2xl text-primaryDark'>
-              Restablecer contraseña
-            </h3>
-            <h4 className='font text-lg'>Introducir contraseña</h4>
-          </div>
-          <form onSubmit={handleForm} className='flex flex-col'>
-            <label className='text-sm' htmlFor='password'>
-              contraseña
-            </label>
-            <input
-              value={password}
-              onChange={(e) => {
-                setFormData(setPassword(e.target.value));
-              }}
-              required
-              id='password'
-              type='password'
-              placeholder='Contraseña'
-              className='mb-2 border-b outline-none py-1'
-            />
-            <label className='text-sm mt-5' htmlFor='repeatPassword'>
-              repetir contraseña
-            </label>
-            <input
-              value={repeatPassword}
-              onChange={(e) => {
-                handleRepeatPassword(e);
-              }}
-              required
-              id='repeatPassword'
-              type='password'
-              placeholder='Repetir contraseña'
-              className='border-b outline-none py-1'
-              onBlur={() => {
-                setOnBlurRepeatPassword(true);
-              }}
-            />
-            {!isRepeatPasswordValid &&
-              onBlurRepeatPassword &&
-              repeatPassword && (
-                <p className='text-sm mt-1 text-red-600'>
-                  Las contraseñas no coinciden
-                </p>
-              )}
-            <button className='md:hover:bg-primaryExtraDark md:transition-colors bg-primaryDark p-2 text-white rounded font-normal'>
-              Restablecer
-            </button>
-          </form>
-        </article>
-      ) : (
-        <article className='flex justify-center'>
-          <div className='m-3 sm:m-5 max-w-6xl w-full'>
-            <h3 className='text-center font-bold text-lg mb-3 sm:mb-5 sm:text-2xl'>
-              Contraseña restaurada con éxito
-            </h3>
-            <div className='flex flex-col items-center'>
-              <motion.div
-                initial={{ scale: 0 }}
-                animate={{ scale: 1 }}
-                transition={{ type: 'spring', stiffness: 100 }}
-                className='w-28 h-28 md:w-36 md:h-36 bg-green-700 p-5 mt-5 mb-3 rounded-full'
-              >
-                <AiFillCheckCircle className='fill-white w-full h-full' />
-              </motion.div>
-            </div>
-          </div>
-        </article>
-      )}
+    <section className="flex justify-center m-7 sm:m-14">
+      <article className=" bg-white rounded-md shadow p-10 w-96">
+        <ToastContainer />
+        <div className="mb-5 border-b-2 pb-2 border-primaryDark">
+          <h3 className="font-medium text-2xl text-primaryDark">
+            Restablecer contraseña
+          </h3>
+          <h4 className="font text-lg">Introducir contraseña</h4>
+        </div>
+        <form onSubmit={handleForm} className="flex flex-col">
+          <label className="text-sm" htmlFor="password">
+            contraseña
+          </label>
+          <input
+            value={password}
+            onChange={(e) => {
+              setFormData(setPassword(e.target.value));
+            }}
+            required
+            id="password"
+            type="password"
+            placeholder="Contraseña"
+            className="mb-2 border-b outline-none py-1"
+          />
+          <label className="text-sm" htmlFor="repeatPassword">
+            repetir contraseña
+          </label>
+          <input
+            value={repeatPassword}
+            onChange={(e) => {
+              handleRepeatPassword(e);
+            }}
+            required
+            id="repeatPassword"
+            type="password"
+            placeholder="Repetir contraseña"
+            className="border-b outline-none py-1"
+            onBlur={() => {
+              setOnBlurRepeatPassword(true);
+            }}
+          />
+          {!isRepeatPasswordValid && onBlurRepeatPassword && repeatPassword && (
+            <p className="text-sm mt-1 text-red-600">
+              Las contraseñas no coinciden
+            </p>
+          )}
+          <button className="flex justify-center items-center md:hover:bg-primaryExtraDark md:transition-colors bg-primaryDark p-2 mt-5 text-white rounded font-normal">
+            {loading ? (
+              <ImSpinner8 className="animate-spin w-7 h-7" />
+            ) : (
+              "Restablecer"
+            )}
+          </button>
+        </form>
+      </article>
     </section>
   );
 }
