b61cb0e5a5b119bb2a059ca5365fb1e2decd0215
Tobias <tobyajras@gmail.com>
2023-11-07 14:14:26 -0300

    image max height fixed, product details

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -170,7 +170,7 @@ function ProductDetails() {
                       className='flex justify-center lg:justify-start items-center px-5 py-10 sm:px-20 md:px-14 lg:px-28 lg:cursor-zoom-in'
                     >
                       <img
-                        className='sm:min-w-[250px] md:min-w-[220px] lg:max-w-[370px] '
+                        className='min-w-[200px] sm:min-w-[250px] lg:max-w-[370px] md:max-h-[300px]'
                         src={productFoundOne.toString()}
                         alt={productFoundOne.toString()}
                         ref={imageRef}
